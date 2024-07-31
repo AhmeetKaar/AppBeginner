c57ff0df0679f6a76d926424feb4e396191d46ff
AhmeetKaar <ahmetkar415@gmail.com>
2024-07-31 16:20:35 +0300

    rest apı learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:beginnerapp/model/user_model.dart';
+import 'package:beginnerapp/service/user_service.dart';
 
-void main() {
-  runApp(const MyApp()); 
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'To Do List', 
-      theme: ThemeData.dark(),
-      home: const MyHomePage(title: 'To Do List',), 
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title,});
+class _MyAppState extends State<MyApp> {
+  UserService _service = UserService();
+  bool? isLoading;
 
-  final String title;
-  
+  List<UsersModelData?> users = [];
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  // ignore: unused_element
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  void initState() {
+    super.initState();
+    _service.fetchUsers().then((value) {
+      if (value != null && value.data != null) {
+        setState(() {
+          users = value.data!;
+          isLoading = true;
+        });
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      appBar: AppBar(   
-        title: Text(widget.title),   
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('SAYAÇ:',),
-            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium,),
-          ],
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('User Information'),
         ),
+        body: isLoading == null
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : isLoading == true ?  ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                        users[index]!.firstName! + users[index]!.lastName!),
+                    subtitle: Text(users[index]!.email!),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(users[index]!.avatar!),
+                    ),
+                  );
+                },
+              ): const Center(
+                child: Text("Bir sorun oluştu.."),
+              ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:(){
-          setState(() {
-            _counter += 1;
-          });
-        },
-        child: const Text('ADD'),
-      ), // 
     );
   }
 }
-
